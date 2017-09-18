1a7600466cab4bdf4145f38756edafec77c7199a
= <anders.selborn@wspgroup.se>
2017-09-18 14:15:46 +0200

    Fixing POM

--- src/main/java/img/Impl/ImgServiceImpl.java
+++ src/main/java/img/Impl/ImgServiceImpl.java
@@ -2,10 +2,22 @@ package img.Impl;
 
 import img.ImgService;
 import img.PingResponse;
+import img.ResponseMessage;
+
+import javax.ws.rs.core.Response;
+
 
 public class ImgServiceImpl implements ImgService {
 
+    @Override
     public PingResponse ping() {
         return new PingResponse("This is a successmessage!");
+    }
+
+    @Override
+    public Response ping2() {
+
+        return Response.status(200).entity(new PingResponse("OK!")).build();
+
     }
 }
